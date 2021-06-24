db7fa7fb7e3b9728ab01955c7849ef8f1058333d
bunnyDrug <1652666+bunnyDrug@users.noreply.github.com>
2021-06-24 22:50:34 +0100

    3.2.3

--- Chapter-3/3.2.3-passing-parameters/main.cu
+++ Chapter-3/3.2.3-passing-parameters/main.cu
@@ -5,10 +5,20 @@ __global__ void add ( int a, int b, int *c ) {
     *c = a + b;
 }
 
-int main( void ) {
+int main() {
     int c;
     int *dev_c;
 
-    HANDLE_ERROR(cudaMalloc( (void**) &dev_c, sizeof(int) ) );
+    // allocate gpu memory to the size of int - pointer in dev_c
+    HANDLE_ERROR(cudaMalloc( &dev_c, sizeof(int) ) );
+
+    add<<<1,1>>>( 2, 7, dev_c);
+
+    HANDLE_ERROR(cudaMemcpy( &c, dev_c, sizeof(int), cudaMemcpyDeviceToHost) );
+
+    printf( "2 + 7 = %d\n", c );
+    cudaFree( dev_c );
+
+    return 0;
 }
 
