03d91d4f699e1d623eaf2703735270899a91883c
bunnyDrug <1652666+bunnyDrug@users.noreply.github.com>
2023-01-14 23:53:16 +0000

    Fixed error with prop type + ternary op

--- Chapter-3/3.2.4-device-properties/main.cu
+++ Chapter-3/3.2.4-device-properties/main.cu
@@ -1,5 +1,6 @@
 #include "../../imp/common/book.h"
-
+#include <cuda.h>
+#include <cuda_runtime_api.h>
 
 int main() {
     cudaDeviceProp prop{};
@@ -11,24 +12,12 @@ int main() {
 
         // do something with the device properties
         printf("   --- General Information for device %d ---\n", i);
-        printf("Name: %s\n", prop.name);
-        printf("Compute capability: %d.%d\n", prop.major, prop.minor);
+        printf("Name: %s\n",                    prop.name);
+        printf("Compute capability: %d.%d\n",   prop.major, prop.minor);
         printf("Clock rate: %d\n", prop.clockRate);
-
-        printf("Device copy overlap:  ");
-        if (prop.deviceOverlap) {
-            printf("Enabled\n");
-        } else {
-            printf("Disabled\n");
-        }
-
-        printf("Kernel execution timeout: ");
-        if (prop.kernelExecTimeoutEnabled) {
-            printf("Enabled\n");
-        } else {
-            printf("Disabled\n");
-        }
-
+        printf("Device copy overlap: %s\n", (prop.deviceOverlap ? ("Enabled") : ("Disabled")));
+        printf("Kernel execution timeout: %s\n", (prop.kernelExecTimeoutEnabled ? ("Enabled") : ("Disabled")));
+        
         printf("   --- Memory Information for device %d ---\n", i);
         printf("Total global memory: %ld\n", prop.totalGlobalMem);
         printf("Total constant memory: %ld\n", prop.totalConstMem);
@@ -37,16 +26,18 @@ int main() {
 
         printf("   --- MP Information for device %d ---\n", i);
         printf("Multiprocessor count: %d\n", prop.multiProcessorCount);
-        printf("Shared mem p/mp: %d\n", prop.sharedMemPerMultiprocessor);
+        printf("Shared mem p/mp: %ld\n", prop.sharedMemPerMultiprocessor);
         printf( "Shared mem per mp:  %ld\n", prop.sharedMemPerBlock );
         printf( "Registers per mp:  %d\n", prop.regsPerBlock );
         printf( "Threads in warp:  %d\n", prop.warpSize );
         printf( "Max threads per block:  %d\n",prop.maxThreadsPerBlock );
-        printf( "Max thread dimensions:  (%d, %d, %d)\n",thene
-                prop.maxThreadsDim[0], prop.maxThreadsDim[1],
+        printf( "Max thread dimensions:  (%d, %d, %d)\n",
+                prop.maxThreadsDim[0],
+                prop.maxThreadsDim[1],
                 prop.maxThreadsDim[2] );
         printf( "Max grid dimensions:  (%d, %d, %d)\n",
-                prop.maxGridSize[0], prop.maxGridSize[1],
+                prop.maxGridSize[0],
+                prop.maxGridSize[1],
                 prop.maxGridSize[2] );
         printf( "\n" );
     }
